d93de99c0c486850c53803e6a3ae61ee6e37f416
sangarshanan <sangarshanan1998@gmail.com>
2021-11-06 00:43:21 +0530

    Add digital root

---
+++ java/DescendingOrder.java
@@ -0,0 +1,27 @@
+/*
+
+Your task is to make a function that can take any non-negative integer as an argument
+and return it with its digits in descending order.
+Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321
+
+
+*/
+
+public class DescendingOrder {
+    public static int sortDesc(final int num) {
+        //Your code
+        System.out.println(num.length());
+        return 1;
+  }
+  
+    public static void main(String[] args) {
+        System.out.println(DescendingOrder.sortDesc(42145));  
+  }
+}
