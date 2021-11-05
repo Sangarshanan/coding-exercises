9c17679ac640e2f30b047ecd15593d01724dda41
sangarshanan <sangarshanan1998@gmail.com>
2021-11-06 01:06:59 +0530

    DescendingOrder.java

--- java/DescendingOrder.java
+++ java/DescendingOrder.java
@@ -11,17 +11,51 @@ Input: 145263 Output: 654321
 
 Input: 123456789 Output: 987654321
 
-
+Run: PROGRAM=DescendingOrder && javac $PROGRAM.java && java $PROGRAM
 */
 
 public class DescendingOrder {
-    public static int sortDesc(final int num) {
-        //Your code
-        System.out.println(num.length());
-        return 1;
+  public static int sortDesc(final int num) {
+    int sortedNumber = 0;
+    for (int i = 9; i >= 0; i--) {
+        int tmpNumber = num;
+
+        while (tmpNumber > 0) {
+            int digit = tmpNumber % 10;
+
+            if (digit == i) {
+                sortedNumber *= 10;
+                sortedNumber += digit;
+            }
+            tmpNumber /= 10;
+        }
+    }
+    // return our sorted number
+    return sortedNumber;
   }
-  
     public static void main(String[] args) {
-        System.out.println(DescendingOrder.sortDesc(42145));  
-  }
+        // Should be all True
+        System.out.println(54321 == DescendingOrder.sortDesc(12345));
+        System.out.println(654321 == DescendingOrder.sortDesc(145263));
+    }
 }
+
+
+/*
+
+CleverSolution:
+
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+public class DescendingOrder {
+    public static int sortDesc(final int num) {
+        return Integer.parseInt(String.valueOf(num)
+                                      .chars()
+                                      .mapToObj(i -> String.valueOf(Character.getNumericValue(i)))
+                                      .sorted(Comparator.reverseOrder())
+                                      .collect(Collectors.joining()));
+    }
+}
+
+*/
